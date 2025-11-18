f972c7de64b3a7aa6654f2f8eb80b49772041f8b
Academic Purposes <lazarokirbyandrei.c@gmail.com>
2025-11-18 09:46:05 +0800

    Add project files.

---
+++ EDP-LazaroSTEProblem1/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TBArtworkTitle = New System.Windows.Forms.TextBox()
+        Me.ArtistName = New System.Windows.Forms.TextBox()
+        Me.ArtworkPrice = New System.Windows.Forms.TextBox()
+        Me.CategoryComboBox = New System.Windows.Forms.ComboBox()
+        Me.AddArtwork = New System.Windows.Forms.Button()
+        Me.AnalyzeExhibit = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ResetData = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Exhibit = New System.Windows.Forms.ListBox()
+        Me.Outputs = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TBArtworkTitle
+        '
+        Me.TBArtworkTitle.Location = New System.Drawing.Point(3, 152)
+        Me.TBArtworkTitle.Name = "TBArtworkTitle"
+        Me.TBArtworkTitle.Size = New System.Drawing.Size(78, 20)
+        Me.TBArtworkTitle.TabIndex = 0
+        '
+        'ArtistName
+        '
+        Me.ArtistName.Location = New System.Drawing.Point(87, 152)
+        Me.ArtistName.Name = "ArtistName"
+        Me.ArtistName.Size = New System.Drawing.Size(78, 20)
+        Me.ArtistName.TabIndex = 1
+        '
+        'ArtworkPrice
+        '
+        Me.ArtworkPrice.Location = New System.Drawing.Point(171, 152)
+        Me.ArtworkPrice.Name = "ArtworkPrice"
+        Me.ArtworkPrice.Size = New System.Drawing.Size(78, 20)
+        Me.ArtworkPrice.TabIndex = 2
+        '
+        'CategoryComboBox
+        '
+        Me.CategoryComboBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CategoryComboBox.FormattingEnabled = True
+        Me.CategoryComboBox.Items.AddRange(New Object() {"Painting", "Sculpture", "Digital Art", "Photography", "Crafts", " "})
+        Me.CategoryComboBox.Location = New System.Drawing.Point(255, 152)
+        Me.CategoryComboBox.Name = "CategoryComboBox"
+        Me.CategoryComboBox.Size = New System.Drawing.Size(120, 21)
+        Me.CategoryComboBox.TabIndex = 3
+        '
+        'AddArtwork
+        '
+        Me.AddArtwork.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.AddArtwork.Location = New System.Drawing.Point(3, 301)
+        Me.AddArtwork.Name = "AddArtwork"
+        Me.AddArtwork.Size = New System.Drawing.Size(78, 146)
+        Me.AddArtwork.TabIndex = 4
+        Me.AddArtwork.Text = "Add Artwork"
+        Me.AddArtwork.UseVisualStyleBackColor = True
+        '
+        'AnalyzeExhibit
+        '
+        Me.AnalyzeExhibit.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.AnalyzeExhibit.Location = New System.Drawing.Point(381, 301)
+        Me.AnalyzeExhibit.Name = "AnalyzeExhibit"
+        Me.AnalyzeExhibit.Size = New System.Drawing.Size(279, 146)
+        Me.AnalyzeExhibit.TabIndex = 5
+        Me.AnalyzeExhibit.Text = "Analyze Exhibit"
+        Me.AnalyzeExhibit.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 6
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.58245!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.58245!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.58245!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.87368!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.71577!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.6632!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ResetData, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TBArtworkTitle, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.ArtistName, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.ArtworkPrice, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.AddArtwork, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.CategoryComboBox, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label4, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.AnalyzeExhibit, 4, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Exhibit, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Outputs, 5, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(800, 450)
+        Me.TableLayoutPanel1.TabIndex = 7
+        '
+        'ResetData
+        '
+        Me.ResetData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ResetData.Location = New System.Drawing.Point(87, 301)
+        Me.ResetData.Name = "ResetData"
+        Me.ResetData.Size = New System.Drawing.Size(78, 146)
+        Me.ResetData.TabIndex = 11
+        Me.ResetData.Text = "Reset Data"
+        Me.ResetData.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label3.Location = New System.Drawing.Point(171, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Price"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label2.Location = New System.Drawing.Point(87, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Artist"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label1.Location = New System.Drawing.Point(3, 136)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Artwork Title"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label4.Location = New System.Drawing.Point(255, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Category"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Exhibit
+        '
+        Me.Exhibit.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Exhibit.FormattingEnabled = True
+        Me.Exhibit.Location = New System.Drawing.Point(381, 3)
+        Me.Exhibit.Name = "Exhibit"
+        Me.TableLayoutPanel1.SetRowSpan(Me.Exhibit, 2)
+        Me.Exhibit.Size = New System.Drawing.Size(279, 292)
+        Me.Exhibit.TabIndex = 12
+        '
+        'Outputs
+        '
+        Me.Outputs.AutoSize = True
+        Me.Outputs.Location = New System.Drawing.Point(666, 0)
+        Me.Outputs.Name = "Outputs"
+        Me.Outputs.Size = New System.Drawing.Size(52, 13)
+        Me.Outputs.TabIndex = 13
+        Me.Outputs.Text = "OUTPUT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TBArtworkTitle As TextBox
+    Friend WithEvents ArtistName As TextBox
+    Friend WithEvents ArtworkPrice As TextBox
+    Friend WithEvents CategoryComboBox As ComboBox
+    Friend WithEvents AddArtwork As Button
+    Friend WithEvents AnalyzeExhibit As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ResetData As Button
+    Friend WithEvents Exhibit As ListBox
+    Friend WithEvents Outputs As Label
+End Class
---
+++ EDP-LazaroSTEProblem1/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Dim ArtworkList As New List(Of String)
+    Private Sub AddArtwork_Click(sender As Object, e As EventArgs) Handles AddArtwork.Click
+        Try
+            Dim artworkTitle As String = TBArtworkTitle.Text
+            Dim artist As String = ArtistName.Text
+            Dim category As String = CategoryComboBox.Text
+            Dim price As Double
+            Dim check As Boolean = Double.TryParse(ArtworkPrice.Text, price)
+
+            If String.IsNullOrEmpty(artworkTitle) Or String.IsNullOrEmpty(artist) Or Not check Or String.IsNullOrEmpty(category) Then
+                MsgBox("Try Again: Empty Field or Invalid Price")
+                Return
+            End If
+
+            ArtworkList.Add(artworkTitle & " | " & artist & " | " & category & " | " & price)
+
+            TBArtworkTitle.Clear()
+            ArtistName.Clear()
+            CategoryComboBox.SelectedIndex = -1
+            ArtworkPrice.Clear()
+
+        Catch ex As Exception
+            MsgBox("Something Went Wrong: " & ex.Message)
+        End Try
+    End Sub
+    Private Sub ResetData_Click(sender As Object, e As EventArgs) Handles ResetData.Click
+        ArtworkList.Clear()
+        Exhibit.Items.Clear()
+    End Sub
+    Private Sub AnalyzeExhibit_Click(sender As Object, e As EventArgs) Handles AnalyzeExhibit.Click
+        Exhibit.Items.Clear()
+
+        If ArtworkList.Count = 0 Then
+            Outputs.Text = "No artworks recorded."
+            Return
+        End If
+
+        Dim totalArtworks As Integer = ArtworkList.Count
+        Dim totalPrice As Double = 0
+        Dim highestPrice As Double = Double.MinValue
+        Dim lowestPrice As Double = Double.MaxValue
+        Dim categoryCounts As New Dictionary(Of String, Integer)
+
+        For Each art As String In ArtworkList
+            Exhibit.Items.Add(art)
+
+            Dim parts() As String = art.Split("|"c)
+            Dim category As String = parts(2).Trim()
+            Dim price As Double = Double.Parse(parts(3).Trim())
+
+            totalPrice += price
+            If price > highestPrice Then highestPrice = price
+            If price < lowestPrice Then lowestPrice = price
+
+            If categoryCounts.ContainsKey(category) Then
+                categoryCounts(category) += 1
+            Else
+                categoryCounts(category) = 1
+            End If
+        Next
+
+        Dim outputText As String = "Total Artworks: " & totalArtworks & Environment.NewLine
+        outputText &= "Average Price: " & (totalPrice / totalArtworks).ToString("F2") & Environment.NewLine
+        outputText &= "Highest Price: " & highestPrice.ToString("F2") & Environment.NewLine
+        outputText &= "Lowest Price: " & lowestPrice.ToString("F2") & Environment.NewLine
+        outputText &= "Artworks per Category:" & Environment.NewLine
+
+        For Each kvp In categoryCounts
+            outputText &= kvp.Key & ": " & kvp.Value & Environment.NewLine
+        Next
+
+        Outputs.Text = outputText
+    End Sub
+End Class
